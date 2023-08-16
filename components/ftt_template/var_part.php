2994022205fe7cf6fe62c3791852636721c14f8e
Zhicha <constellationnew@gmai.com>
2023-08-16 19:06:14 +0300

    ver 11.4.6 Шаблон, календарь

--- components/ftt_template/var_part.php
+++ components/ftt_template/var_part.php
@@ -1,97 +1,10 @@
 <?php
-  // СПИСОК ПВОМ
-  // DB
-  include_once 'db/ftt/ftt_list_db.php';
-  // Classes
-  include_once 'db/classes/localities.php';
-  include_once 'db/classes/member_properties.php';
-  include_once 'db/classes/members.php';
-  //include_once 'db/classes/ftt_info.php';
-
-// access
-if ($ftt_access['group'] !== 'staff' && $ftt_access['group'] !== 'trainee') {
-  exit();
-}
-
-// данные обучающегося
-$trainee_data = trainee_data::get_data($memberId);
-$serving_trainee = '';
-// служащие из обучающихся
-if (isset($ftt_access['ftt_service']) && $ftt_access['ftt_service'] === '06') {
-  $serving_trainee = 1;
-}
-
-// Координатор
-if (isset($trainee_data['coordinator']) && $trainee_data['coordinator'] === '1' && $ftt_access['ftt_service'] !== '06') {
-  $serving_trainee = 1;
-}
-
-
-$serving_ones_list = ftt_lists::serving_ones();
-// $serving_ones_list_full = ftt_lists::serving_ones_full();
-$serving_ones_list_list = ftt_lists::serving_ones_list();
-
-$trainee_list = ftt_lists::trainee();
-// $trainee_list_full = ftt_lists::trainee_full();
-$trainee_list_list = ftt_lists::trainee_list();
-
-$categories_list = MemberProperties::get_categories();
-// список из зоны ответственности админа
-// Ислючить при запросе тех кто в списке обучающихся
-// Members::db_get_members_by_admin('000001679');
-$localities = [];
-$localities_staff = [];
-
-foreach ($trainee_list_list as $key => $value) {
-  $localities[$value['locality_key']] = $value['locality_name'];
-}
-
-foreach ($serving_ones_list_list as $key => $value) {
-  $localities_staff[$value['locality_key']] = $value['locality_name'];
-}
-
-// СОРТИРОВКИ
-// Местности
-asort($localities);
-asort($localities_staff);
+// РАЗДЕЛ
+// DB
+//include_once 'db/ftt/ftt_list_db.php';
+// Classes
+//include_once 'db/classes/members.php';
 
 // Списки
-// Обучающиеся
-$sort_icon = "<i class='fa fa-caret-down'></i>";
-$sort_field = "name";
-if (isset($_COOKIE['sorting']) && isset($_COOKIE['desc']) && $_COOKIE['desc'] === "1") {
-  $sort_field = $_COOKIE['sorting'];
-  usort($trainee_list_list, function ($item1, $item2) {
-      return $item1[$_COOKIE['sorting']] <=> $item2[$_COOKIE['sorting']];
-  });
-} else if (isset($_COOKIE['sorting']) && isset($_COOKIE['desc']) && $_COOKIE['desc'] === "0") {
-  $sort_field = $_COOKIE['sorting'];
-  $sort_icon = "<i class='fa fa-caret-up'></i>";
-  usort($trainee_list_list, function ($item1, $item2) {
-      return $item2[$_COOKIE['sorting']] <=> $item1[$_COOKIE['sorting']];
-  });
-}
-
-// Служащие
-$sort_icon_staff = "<i class='fa fa-caret-down'></i>";
-$sort_field_staff = "name";
-if (isset($_COOKIE['sorting_staff']) && isset($_COOKIE['desc_staff']) && $_COOKIE['desc_staff'] === "1") {
-  $sort_field_staff = $_COOKIE['sorting_staff'];
-  usort($serving_ones_list_list, function ($item1, $item2) {
-      return $item1[$_COOKIE['sorting_staff']] <=> $item2[$_COOKIE['sorting_staff']];
-  });
-} else if (isset($_COOKIE['sorting_staff']) && isset($_COOKIE['desc_staff']) && $_COOKIE['desc_staff'] === "0") {
-  $sort_field_staff = $_COOKIE['sorting_staff'];
-  $sort_icon_staff = "<i class='fa fa-caret-up'></i>";
-  usort($serving_ones_list_list, function ($item1, $item2) {
-      return $item2[$_COOKIE['sorting_staff']] <=> $item1[$_COOKIE['sorting_staff']];
-  });
-}
-
-$tab_active = "tab_trainee";
-
-if (isset($_COOKIE['tab_selected'])) {
-  $tab_active = $_COOKIE['tab_selected'];
-}
-
-?>
+// $serving_ones_list_full = ftt_lists::serving_ones_full();
+// $trainee_list_full = ftt_lists::trainee_full();
--- js/ftt/ftt_fellowship/script.js
+++ js/ftt/ftt_fellowship/script.js
@@ -382,5 +382,141 @@ $(document).ready(function(){
     });
   }
   */
+  // CALENDAR
+  /*
+  let html = '<div class="calendar-wrapper">
+    	<button id="btnPrev" type="button">Предыдущий</button>
+    	<button id="btnNext" type="button">Следующий</button>
+		<div id="divCal"></div>';
+  let Cal = function(divId) {
+    //Сохраняем идентификатор div
+    this.divId = divId;
+    // Дни недели с понедельника
+    this.DaysOfWeek = [
+      'Пн',
+      'Вт',
+      'Ср',
+      'Чт',
+      'Пт',
+      'Сб',
+      'Вс'
+    ];
+    // Месяцы начиная с января
+    this.Months =['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    //Устанавливаем текущий месяц, год
+    let d = new Date();
+    this.currMonth = d.getMonth();
+    this.currYear = d.getFullYear();
+    this.currDay = d.getDate();
+  };
+  // Переход к следующему месяцу
+  Cal.prototype.nextMonth = function() {
+    if ( this.currMonth == 11 ) {
+      this.currMonth = 0;
+      this.currYear = this.currYear + 1;
+    }
+    else {
+      this.currMonth = this.currMonth + 1;
+    }
+    this.showcurr();
+  };
+  // Переход к предыдущему месяцу
+  Cal.prototype.previousMonth = function() {
+    if ( this.currMonth == 0 ) {
+      this.currMonth = 11;
+      this.currYear = this.currYear - 1;
+    }
+    else {
+      this.currMonth = this.currMonth - 1;
+    }
+    this.showcurr();
+  };
+  // Показать текущий месяц
+  Cal.prototype.showcurr = function() {
+    this.showMonth(this.currYear, this.currMonth);
+  };
+  // Показать месяц (год, месяц)
+  Cal.prototype.showMonth = function(y, m) {
+    let d = new Date()
+    // Первый день недели в выбранном месяце
+    , firstDayOfMonth = new Date(y, m, 7).getDay()
+    // Последний день выбранного месяца
+    , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
+    // Последний день предыдущего месяца
+    , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+    let html = '<table>';
+    // Запись выбранного месяца и года
+    html += '<thead><tr>';
+    html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
+    html += '</tr></thead>';
+    // заголовок дней недели
+    html += '<tr class="days">';
+    for(let i=0; i < this.DaysOfWeek.length;i++) {
+      html += '<td>' + this.DaysOfWeek[i] + '</td>';
+    }
+    html += '</tr>';
+    // Записываем дни
+    let i=1;
+    do {
+      let dow = new Date(y, m, i).getDay();
+      // Начать новую строку в понедельник
+      if ( dow == 1 ) {
+        html += '<tr>';
+      }
+      // Если первый день недели не понедельник показать последние дни предыдущего месяца
+      else if ( i == 1 ) {
+        html += '<tr>';
+        let k = lastDayOfLastMonth - firstDayOfMonth+1;
+        for(let j=0; j < firstDayOfMonth; j++) {
+          html += '<td class="not-current">' + k + '</td>';
+          k++;
+        }
+      }
+      // Записываем текущий день в цикл
+      let chk = new Date();
+      let chkY = chk.getFullYear();
+      let chkM = chk.getMonth();
+      if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
+        html += '<td class="today record_available" data-date="' + chkY + '-' + (chkM < 10 ? '0' + String(chkM) : chkM)  + '-' + i + '">' + i + '</td>';
+      } else {
+        html += '<td class="normal" data-date="' + chkY + '-' + (chkM < 10 ? '0' + String(chkM) : chkM) + '-' + i + '">' + i + '</td>';
+      }
+      // закрыть строку в воскресенье
+      if (dow == 0) {
+        html += '</tr>';
+      }
+      // Если последний день месяца не воскресенье, показать первые дни следующего месяца
+      else if ( i == lastDateOfMonth ) {
+        let k=1;
+        for(dow; dow < 7; dow++) {
+          html += '<td class="not-current">' + k + '</td>';
+          k++;
+        }
+      }
+      i++;
+    }while(i <= lastDateOfMonth);
+    // Конец таблицы
+    html += '</table>';
+    // Записываем HTML в div
+    document.getElementById(this.divId).innerHTML = html;
+  };
+  // При загрузке окна
+  window.onload = function() {
+    // Начать календарь
+    let c = new Cal("divCal");
+    c.showcurr();
+    // Привязываем кнопки «Следующий» и «Предыдущий»
+    getId('btnNext').onclick = function() {
+      c.nextMonth();
+    };
+    getId('btnPrev').onclick = function() {
+      c.previousMonth();
+    };
+  }
+  // Получить элемент по id
+  function getId(id) {
+    return document.getElementById(id);
+  }
+  */
   /*** FELLOWSHIP TAB STOP ***/
 });
