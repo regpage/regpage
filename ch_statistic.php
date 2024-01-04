20503660674868106670599ee7c87abfae99bfff
zhicha <zhichkinroman@gmail.com>
2024-01-04 20:02:24 +0300

    ver 12.0.2 статистика

---
+++ ch_statistic.php
@@ -0,0 +1,20 @@
+<?php
+    require_once "preheader.php";
+
+    include_once "header2.php";
+    // Меню
+    include_once "nav2.php";
+
+    // РАСПИСАНИЕ Переменные раздела
+    include_once "components/regpage/ch_statistic/var_part.php";
+
+    // HTML код основной страницы
+    include_once "components/regpage/main/content_container.php";
+
+    // HTML модальные окна основной страницы
+    include_once "components/regpage/ch_statistic/modals_part.php";
+
+    // JS
+    include_once "components/regpage/ch_statistic/js_part.php";
+
+    include_once "footer2.php";
