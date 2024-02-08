d9160a040d76cab071c8b38dc8b01f37129390e3
zhicha <zhichkinroman@gmail.com>
2024-02-08 15:28:36 +0300

    ver 12.1.6 баг дизайна в разделе списки посещаемости на узких экранах

--- js/regpage/attend/design.js
+++ js/regpage/attend/design.js
@@ -17,5 +17,17 @@ if ($(window).width()<=769) {
   // rows
   $(".attend_str").removeClass("pl-1");
   $(".attend_str .col-3:nth-child(1)").addClass("pr-1");
-  $(".attend_str .col-3:nth-child(2)").addClass("pr-1").addClass("pl-1");  
+  $(".attend_str .col-3:nth-child(2)").addClass("pr-1").addClass("pl-1");
+} else if ($(window).width() > 769 && $(window).width() <= 1199) {
+  $("#attend_list").addClass("mx-1");
+  $(".attend_str .data_name").parent().attr("style", "max-width:160px;");
+  $(".attend_str .data_name").parent().next().attr("style", "max-width:160px;");
+  if ($(window).width() < 1001) {
+    $("#col_name").hide();
+    $(".attend_str .data_age").parent().attr("style", "max-width:30px;");
+    $(".attend_str .vt_comment_text").parent().attr("style", "max-width:60px;");    
+  } else {
+    $("#col_name b[data-sort='name']").parent().attr("style", "max-width:160px;");
+    $("#col_name b[data-sort='locality']").parent().attr("style", "max-width:160px;");
+  }
 }
