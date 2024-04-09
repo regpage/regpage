10ad3c8f90580e7e7f07b14c04bb8ffa3c80d72e
zhicha <zhichkinroman@gmail.com>
2024-04-09 13:07:05 +0300

    v 12.4.5  баг

--- components/ftt_attendance/js_part.php
+++ components/ftt_attendance/js_part.php
@@ -79,10 +79,10 @@ if ($(window).width()<=769) {
   $(".row_meet").hide();
 }
 </script>
-<script src="/js/ftt/ftt_attendance/script.js?v195"></script>
+<script src="/js/ftt/ftt_attendance/script.js?v196"></script>
 <script src="/js/ftt/ftt_attendance/design.js?v34"></script>
 <script src="/js/modules/week.js?v1"></script>
 <script src="/js/modules/time.js?v1"></script>
 <script src="/js/modules/date.js?v3"></script>
 <script src="/js/modules/blank.js?v1"></script>
-<script src="/js/modules/ftt/bible_read.js?v2"></script>
+<script src="/js/modules/ftt/bible_read.js?v3"></script>
