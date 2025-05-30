699a40e1433a2684d84ac56a554ca65d0dee7461
zhicha <zhichkinroman@gmail.com>
2025-05-30 13:15:47 +0300

    v14.0.0

--- cronprophecy.php
+++ cronprophecy.php
@@ -1,44 +1,48 @@
 <?php
 // **** ВОСКРЕСНЫЙ КРОН проверка бланков пророчествования cronprophecy.php ****//
 // право доступа
-require_once 'cronkey.php';
+//require_once 'cronkey.php';
 
 // нужные классы
 include_once 'config.php';
+include_once 'db/classes/ftt_info.php';
 
 function prophecyCheck()
 {
-  $result = [];
-  $res = db_query("SELECT ft.member_key, fp.send_date
-    FROM  ftt_trainee ft
-    LEFT JOIN ftt_prophecy fp ON fp.member_key = ft.member_key
-    WHERE DATE_FORMAT(fp.send_date, '%Y-%m-%d') = CURDATE()");
-  while ($row = $res->fetch_assoc()) $result[]=$row['member_key'];
+  // добавить в отчёт об исполнении кронов
+  // поведение на перерыве
+  if (ftt_info::pause()) {
+    // отметка о выполнении
+    db_query("INSERT INTO `cron` (`date`,`script`, `status`, `comment`) VALUES (CURRENT_DATE(),'{$_SERVER['PHP_SELF']}', '1', 'Вне периода')");
+    echo "Вне периода проведения обучения";
+    exit();
+  }
 
+  $traineeKeysToCreateExtrahelp = [];
+  // получаем тех кто не на паузе и не отправил бланк на текущее число
+  $res = db_query("SELECT ft.member_key FROM ftt_trainee ft WHERE NOT EXISTS (SELECT fp.member_key FROM ftt_prophecy fp WHERE ft.member_key = fp.member_key AND DATE_FORMAT(fp.send_date, '%Y-%m-%d') = CURDATE()) AND (ft.pause_start IS NULL OR ft.pause_start > CURDATE() OR ft.pause_stop < CURDATE())");
+  while ($row = $res->fetch_assoc()) $traineeKeysToCreateExtrahelp[]=$row['member_key'];
 
-  $condition = '';
-  foreach ($result as $value) {
-    if (empty($condition)) {
-      $condition = "'{$value}'";
-    } else {
-      $condition .= ",'{$value}'";
+  // нет ключе обучающихся для создания доп помощи
+  if (count($traineeKeysToCreateExtrahelp) === 0) {
+    echo "Не добавлены доп. задания при проверке бланко пророчествования";
+    exit();
+  }
+
+  // нет ключе обучающихся для создания доп помощи
+  if (count($traineeKeysToCreateExtrahelp) > 0) {
+    echo "Проверка сдачи бланка пророчества (воскресенье до 18:00). Создана доп. помощь (" . count($traineeKeysToCreateExtrahelp) . ") для обучающихся: ";
+    foreach ($traineeKeysToCreateExtrahelp as $value) {
+      $reason = "Не отправлен вовремя бланк пророчествования " . date('d.m');
+      db_query("INSERT INTO ftt_extra_help (`date`, `member_key`, `reason`, `changed`)
+      VALUES (NOW(), '{$value}', '{$reason}', 1)");
+      echo "{$value}, ";
     }
+    // отметка об исполнении
+    db_query("INSERT INTO `cron` (`date`,`script`, `status`, `comment`) VALUES (CURRENT_DATE(),'{$_SERVER['PHP_SELF']}', '1', 'Проверка сдачи бланка пророчества')");
+    exit();
   }
-
-  $extrahelp = [];
-  $res2 = db_query("SELECT member_key FROM ftt_trainee WHERE member_key NOT IN ($condition)");
-  while ($row = $res2->fetch_assoc()) $extrahelp[]=$row['member_key'];
-
-  if (count($extrahelp) > 0) {
-    echo "Проверка сдачи бланка пророчества (воскресенье до 18:00). Создана доп. помощь для обучающихся: ";
-  }
-  foreach ($extrahelp as $value) {
-    echo "{$value}, ";
-    $reason = '';
-    $reason = "Не отправлен вовремя бланк пророчествования " . date('d.m');
-    db_query("INSERT INTO ftt_extra_help (`date`, `member_key`, `reason`, `changed`)
-    VALUES (NOW(), '{$value}', '{$reason}', 1)");
-    echo "{$value}, ";
-  }
+  echo "Непредвиденная ошибка.";
 }
+
 prophecyCheck();
